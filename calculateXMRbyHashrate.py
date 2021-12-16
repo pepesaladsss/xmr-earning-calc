0aa62b44e855edb1c0c367ce3cf83bb3a211385a
pepesaladsss <68161790+pepesaladsss@users.noreply.github.com>
2021-12-16 06:38:31 -0500

    Update calculateXMRbyHashrate.py
    Added descriptions to `simplifyHashrate()`, `getNetworkHashrate()`, `getXMRPrice()`, and `getBlockReward()`, changed `getXMRPrice()` to return a float value instead of a string, with the $ removed from said value.

--- calculateXMRbyHashrate.py
+++ calculateXMRbyHashrate.py
@@ -1,4 +1,7 @@
 def simplifyHashrate(hashrate):
+    '''Simplifies a hashrate value into h/s for simpler calculations. Supports `kh/s`, `mh/s`, `gh/s`, `th/s`, `ph/s`, 
+    `eh/s`, `zh/s`, `yh/s` being in the `hashrate` value and will calculate accordingly based on which one is within the value. 
+    If none are found, it falls back to assuming `hashrate` is in h/s (multiplier value set to 1) | Returns a `float`'''
     from string import ascii_lowercase
     hashMultiplierDictionary = {"kh": 1000,"mh": 1000000,"gh": 1000000000,"th": 1000000000000,"ph": 1000000000000000,"eh": 1000000000000000000,"zh": 1000000000000000000000,"yh": 1000000000000000000000000}
     hashrate = hashrate.lower()
@@ -15,28 +18,31 @@ def simplifyHashrate(hashrate):
     return float(strippedHashrate) * hashMultiplier
 
 def getNetworkHashrate():
+    '''Gets the total XMR network hashrate from `https://2miners.com/xmr-network-hashrate` and then simplifies the value to h/s using `simplifyHashrate()` | Returns a `float`'''
     from bs4 import BeautifulSoup
     from requests import get
     hashrateElement = BeautifulSoup(get('https://2miners.com/xmr-network-hashrate').text, 'lxml').select_one('#app > div.another-pools-block > div > div > div:nth-child(2) > table > tbody > tr:nth-child(2) > td:nth-child(2)')
     return simplifyHashrate(hashrateElement.text)
 
 def getXMRPrice():
+    '''Gets the current XMR price (in USD, not including $ in value) from `https://coinmarketcap.com/currencies/monero/` using `requests.get()` and `bs4.BeautifulSoup()` | Returns a `float`'''
     from bs4 import BeautifulSoup
     from requests import get
     headers = { # Coinmarketcap was being cringe
         "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.113 Safari/537.36"
     }
     priceElement = BeautifulSoup(get('https://coinmarketcap.com/currencies/monero/', headers=headers).text, 'lxml').select_one('#__next > div.bywovg-1.fUzJes > div > div.sc-57oli2-0.comDeo.cmc-body-wrapper > div > div.sc-16r8icm-0.eMxKgr.container > div.n78udj-0.jskEGI > div > div.sc-16r8icm-0.kjciSH.priceSection > div.sc-16r8icm-0.kjciSH.priceTitle > div')
-    return priceElement.text
+    return float(priceElement.text.replace('$', ''))
 
 def getBlockReward(): 
+    '''Gets the current XMR block reward. I'm unsure how to calculate this properly, so I set it to an arbitrary `0.75XMR` until May 2022 where it will be set to a fixed `0.60XMR` and won't require calculation past that point | Returns a `float`'''
     from datetime import datetime
     if datetime.now().year > 2022 or datetime.now().month >= 5 and datetime.now().year == 2022: return 0.6
     else: return 0.75 # Arbitrary number
 
 def calculateEarnings_(yourHashrate, networkHashrate, blockReward, xmrPrice):
     xmrEarnings = yourHashrate / networkHashrate * 720 * blockReward
-    usdEarnings = float(xmrPrice.replace('$', '')) * xmrEarnings
+    usdEarnings = float(xmrPrice) * xmrEarnings
     return f"Approximate Earnings per 24 hours:\nXMR: {xmrEarnings}\nUSD: {usdEarnings}"
 if __name__ == "__main__":
     yourHashrate = input("Your Hashrate (Include KH/s, MH/s, GH/s, etc): ")
